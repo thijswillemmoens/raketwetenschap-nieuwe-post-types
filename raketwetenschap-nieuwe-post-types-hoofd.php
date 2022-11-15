3cd3092c0cacf6e63aa46994ad071f6855f1a87d
Thijs Moens <thijs.willem.moens@gmail.com>
2022-11-15 14:44:20 +0100

    Update met nieuw post type

--- raketwetenschap-nieuwe-post-types-hoofd.php
+++ raketwetenschap-nieuwe-post-types-hoofd.php
@@ -123,4 +123,67 @@ if ( ! function_exists( 'raketwetenschap__nieuwe_post_type_voor_alle_projecten' 
 
 endif; 
 
+
+
+/**
+ * Maak een post type voor alle raadsels.
+ * 
+ * @package Raketwetenschap
+ * @subpackage Plugins/Nieuwe_Post_Types
+ * @since 3.0
+ * @author Thijs Moens <post@thijsmoens.nl>
+ * @link https://thijsmoens.nl
+ */
+if ( ! function_exists( 'raketwetenschap__nieuwe_post_type_voor_alle_raadsels' ) ) :
+
+    function raketwetenschap__nieuwe_post_type_voor_alle_raadsels() {
+ 
+        $labels = array(
+            'name'                => _x( 'Raadsels', 'Post Type General Name', 'raketwetenschap-nieuwe-post-types' ),
+            'singular_name'       => _x( 'Raadsel', 'Post Type Singular Name', 'raketwetenschap-nieuwe-post-types' ),
+            'menu_name'           => __( 'Raadsels', 'raketwetenschap-nieuwe-post-types' ),
+            'parent_item_colon'   => __( 'Parent Raadsel', 'raketwetenschap-nieuwe-post-types' ),
+            'all_items'           => __( 'Alle raadsels', 'raketwetenschap-nieuwe-post-types' ),
+            'view_item'           => __( 'Bekijk het raadsel', 'raketwetenschap-nieuwe-post-types' ),
+            'add_new_item'        => __( 'Voeg nieuw raadsel toe', 'raketwetenschap-nieuwe-post-types' ),
+            'add_new'             => __( 'Nieuwe raadsel', 'raketwetenschap-nieuwe-post-types' ),
+            'edit_item'           => __( 'Wijzig raadsel', 'raketwetenschap-nieuwe-post-types' ),
+            'update_item'         => __( 'Update raadsel', 'raketwetenschap-nieuwe-post-types' ),
+            'search_items'        => __( 'Doorzoek raadsel', 'raketwetenschap-nieuwe-post-types' ),
+            'not_found'           => __( 'Geen raadsel gevonden', 'raketwetenschap-nieuwe-post-types' ),
+            'not_found_in_trash'  => __( 'Geen raadsel gevonden in de prullenbak', 'raketwetenschap-nieuwe-post-types' ),
+        );
+     
+     
+        $parameters = array(
+            'label'               => __( 'raadsel', 'raketwetenschap-nieuwe-post-types' ),
+            'description'         => __( 'Het raadsel met alle info.', 'raketwetenschap-nieuwe-post-types' ),
+            'labels'              => $labels,
+            'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+            'hierarchical'        => false,
+            'public'              => true,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_nav_menus'   => true,
+            'show_in_admin_bar'   => true,
+            'menu_position'       => 8,
+            'menu_icon'           => 'dashicons-bell',
+            'can_export'          => true,
+            'has_archive'         => true,
+            'exclude_from_search' => false,
+            'publicly_queryable'  => true,
+            'capability_type'     => 'post',
+            'show_in_rest' => true,
+    
+        );
+     
+        // Registreer deze raadsel
+        register_post_type( 'raadsel', $parameters );
+ 
+    }
+ 
+    add_action( 'init', 'raketwetenschap__nieuwe_post_type_voor_alle_raadsels', 0 );
+
+endif; 
+
 ?>
--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 Een simpele plugin voor het maken van nieuwe custom post types voor op de websites van Raketwetenschap.
 
 ### Changelog
+- 3.0: Nieuw post type voor Raadsels.
 - 2.2: Dossiers is toch weer collecties en projecten is erbij gekomen.
 - 2.0: Collecties is nu Dossiers.
 - 1.0: Officiele release van de plugin (15 september 2022)
